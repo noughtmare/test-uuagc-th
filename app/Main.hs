a865d1cbc63cb68f91db1104891afbca277ca164
Jaro Reinders <jaro.reinders@gmail.com>
2021-04-03 19:39:05 +0200

    Use string quasi quoter

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# OPTIONS_GHC -ddump-splices #-}
 module Main where
 
 import UU.UUAGC (uuagcTH)
+import Text.RawString.QQ
 
-pure $ either error id $ uuagcTH ["--data"]
-  "DATA Test\n\
-  \  | Test a : Int b : Bool\n\
-  \  | Cool c : String d : {[Int]}\n"
+pure $ either error id $ uuagcTH ["--data"] [r|
+DATA Test
+  | Test a : Int b : Bool
+  | Cool c : String d : {[Int]}
+|]
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
