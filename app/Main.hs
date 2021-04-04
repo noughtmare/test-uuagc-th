f74a2335028df3907a2f42a8ce7fc201b39d29c3
Jaro Reinders <jaro.reinders@gmail.com>
2021-04-04 21:24:49 +0200

    Update uuagc and expand example

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,26 @@
 {-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# OPTIONS_GHC -ddump-splices #-}
 module Main where
 
 import UU.UUAGC (uuagcTH)
 import Text.RawString.QQ
 
-uuagcTH ["haskellsyntax","data"] [r|
+uuagcTH ["haskellsyntax","data","signatures","catas","wrappers","semfuns"] [r|
 data Tree
   | Node left  :: Tree
          right :: Tree
   | Tip  value :: Int
 
+
+attr Tree
+  syn max :: {Int}
+
+sem Tree
+  | Node lhs.max = max @left.max @right.max
+  | Tip  lhs.max = @value
+
 deriving Tree : Show
 |]
 
 main :: IO ()
-main = print (Node (Tip 1) (Node (Tip 2) (Tip 3)))
+main = print $ max_Syn_Tree $ wrap_Tree (sem_Tree (Node (Tip 1) (Node (Tip 2) (Tip 3)))) Inh_Tree
