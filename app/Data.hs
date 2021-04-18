12d3f3982ee46c5ee50e4e3076cc6dcc0f342bae
Jaro Reinders <jaro.reinders@gmail.com>
2021-04-18 22:54:19 +0200

    Show off the various improvements

--- app/Data.hs
+++ app/Data.hs
@@ -5,11 +5,11 @@ module Data where
 import UU.UUAGC (uuagcTH)
 import Text.RawString.QQ
 
-uuagcTH "dat" ["haskellsyntax","data"] [] [r|
-data Tree
-  | Node left  :: Tree
-         right :: Tree
-  | Tip  value :: Int
+uuagcTH ["haskellsyntax","data"] [] [r|
+data Tree a
+  | Node left  :: (Tree {a})
+         right :: (Tree {a})
+  | Tip  value :: {a}
 
 deriving Tree : Show
 |]
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell, QuasiQuotes, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -ddump-splices #-}
 module Main where
 
@@ -6,14 +6,22 @@ import UU.UUAGC (uuagcTH)
 import Text.RawString.QQ
 import Data
 
-uuagcTH "sem" ["haskellsyntax","signatures","catas","wrappers","semfuns"] [dat] [r|
+uuagcTH ["haskellsyntax","signatures","catas","wrappers","semfuns"] [Data.ag] [r|
 attr Tree
-  syn max :: {Int}
+  syn max :: {@a}
 
-sem Tree
+sem Ord {a} => Tree
   | Node lhs.max = max @left.max @right.max
   | Tip  lhs.max = @value
+
+{
+ex :: Tree Int
+ex = Node (Tip 1) (Node (Tip 2) (Tip 3))
+
+maxTree :: Tree Int -> Int
+maxTree x = max_Syn_Tree $ wrap_Tree (sem_Tree x) Inh_Tree
+}
 |]
 
 main :: IO ()
-main = print $ max_Syn_Tree $ wrap_Tree (sem_Tree (Node (Tip 1) (Node (Tip 2) (Tip 3)))) Inh_Tree
+main = print (maxTree ex)
